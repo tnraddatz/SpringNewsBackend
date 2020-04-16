a14eacb58d43423b95ca25452a96a1b73af301b6
tnraddatz <tnraddatz@email.wm.edu>
2020-04-15 22:02:11 -0400

    JWT Tokens

--- src/main/java/com/news/backend/BackendApplication.java
+++ src/main/java/com/news/backend/BackendApplication.java
@@ -2,9 +2,8 @@ package com.news.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @ComponentScan("com.news.backend")
 @SpringBootApplication(scanBasePackages={"com.news.backend"})
